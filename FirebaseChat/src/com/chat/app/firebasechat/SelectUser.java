0d81c068112cd6291ec378eecb5bebae600d16f2
praful-rathore <prafullarathore@gmail.com>
2015-06-17 20:13:50 +0530

    chat functionality

---
+++ FirebaseChat/src/com/chat/app/firebasechat/SelectUser.java
@@ -0,0 +1,25 @@
+package com.chat.app.firebasechat;
+
+import java.util.List;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class SelectUser extends ActionBarActivity implements OnClickListener {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.listuser);
+	}
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
