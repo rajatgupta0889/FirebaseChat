01caf07e6543a84f8b5aef19253e9553140be675
praful-rathore <prafullarathore@gmail.com>
2015-06-18 19:48:15 +0530

    firebase

--- FirebaseChat/src/com/chat/app/firebasechat/MainActivity.java
+++ FirebaseChat/src/com/chat/app/firebasechat/MainActivity.java
@@ -1,18 +1,27 @@
 package com.chat.app.firebasechat;
 
 import android.support.v7.app.ActionBarActivity;
+import android.telephony.TelephonyManager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,21 +39,34 @@ public class MainActivity extends ListActivity {
 
 	private String mUsername;
 	private Firebase mFirebaseRef;
+	private Context context = this;
 	private ValueEventListener mConnectedListener;
 	private ChatListAdapter mChatListAdapter;
-
+	String radioSelect = "Incompatibility";
+	Intent intent;
+	String  group;
+	TextView grp_name;
+	int id;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 
+//		TelephonyManager tManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+//		String uuid = tManager.getDeviceId();
 		// Make sure we have a mUsername
 		setupUsername();
+		id = getIntent().getExtras().getInt("id");
+		group = getIntent().getExtras().getString("group");
+		grp_name=(TextView)findViewById(R.id.textView1);
+		grp_name.setText(group);
+		
 
 		setTitle("Chatting as " + mUsername);
 
 		// Setup our Firebase mFirebaseRef
-		mFirebaseRef = new Firebase(FIREBASE_URL).child("MarilyChat");
+		mFirebaseRef = new Firebase(FIREBASE_URL).child(group);
 
 		// Setup our input methods. Enter key on the keyboard or pushing the
 		// send button
@@ -69,8 +91,120 @@ public class MainActivity extends ListActivity {
 						sendMessage();
 					}
 				});
+		findViewById(R.id.shortlist).setOnClickListener(
+				new View.OnClickListener() {
 
-	}
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						LayoutInflater li = LayoutInflater.from(context);
+						View custom = li.inflate(R.layout.shortlist_dialog,
+								null);
+						AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+								context);
+
+						alertDialogBuilder.setView(custom);
+						alertDialogBuilder.setCancelable(true);
+
+						// create alert dialog
+						final AlertDialog alertDialog = alertDialogBuilder
+								.create();
+						Button cancel = (Button) custom
+								.findViewById(R.id.shortlist_cancel);
+						cancel.setOnClickListener(new OnClickListener() {
+
+							@Override
+							public void onClick(View v) {
+								// TODO Auto-generated method stub
+								alertDialog.cancel();
+							}
+						});
+
+						Button shortlist = (Button) custom
+								.findViewById(R.id.shortlist_shortlist);
+						shortlist.setOnClickListener(new OnClickListener() {
+
+							@Override
+							public void onClick(View v) {
+								// TODO Auto-generated method stub
+								Toast.makeText(MainActivity.this,
+										"Neha is Shortlisted!",
+										Toast.LENGTH_SHORT).show();
+								alertDialog.cancel();
+							}
+						});
+
+						// show it
+						alertDialog.show();
+					}
+
+				});
+		findViewById(R.id.unmatch).setOnClickListener(
+				new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						LayoutInflater li = LayoutInflater.from(context);
+						View custom = li.inflate(R.layout.unmatch_dialog, null);
+						AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+								context);
+
+						alertDialogBuilder.setView(custom);
+						alertDialogBuilder.setCancelable(true);
+
+						// create alert dialog
+						final AlertDialog alertDialog = alertDialogBuilder
+								.create();
+						Button cancel = (Button) custom
+								.findViewById(R.id.unmatch_cancel);
+						cancel.setOnClickListener(new OnClickListener() {
+
+							@Override
+							public void onClick(View v) {
+								// TODO Auto-generated method stub
+								alertDialog.cancel();
+							}
+						});
+
+						RadioGroup radio = (RadioGroup) custom
+								.findViewById(R.id.unmatch_radioGroup);
+						radio.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+							@Override
+							public void onCheckedChanged(RadioGroup group,
+									int checkedId) {
+								// TODO Auto-generated method stub
+
+								if (checkedId == R.id.radio1_incompatibility) {
+									radioSelect = "Incompatibilty";
+								} else if (checkedId == R.id.radio2_language) {
+									radioSelect = "Inappropriate Language";
+								} else if (checkedId == R.id.radio3_profile) {
+									radioSelect = "Fake Profile";
+								}
+							}
+						});
+
+						Button unmatch = (Button) custom
+								.findViewById(R.id.unmatch_unmatch);
+						unmatch.setOnClickListener(new OnClickListener() {
+
+							@Override
+							public void onClick(View v) {
+								// TODO Auto-generated method stub
+								Toast.makeText(MainActivity.this, radioSelect,
+										Toast.LENGTH_SHORT).show();
+								radioSelect = "Incompatibilty";
+								alertDialog.cancel();
+							}
+						});
+
+						// show it
+						alertDialog.show();
+					}
+				});
+
+	};
 
 	@Override
 	public void onStart() {
@@ -138,7 +272,7 @@ public class MainActivity extends ListActivity {
 		String input = inputText.getText().toString();
 		if (!input.equals("")) {
 			// Create our 'model', a Chat object
-			Chat chat = new Chat(input, mUsername, imageurl);
+			Chat chat = new Chat(input, mUsername,  id+""+group);
 			// Create a new, auto-generated child of that chat location, and
 			// save our chat data there
 			mFirebaseRef.push().setValue(chat);
--- FirebaseChat/src/com/chat/app/firebasechat/SelectUser.java
+++ FirebaseChat/src/com/chat/app/firebasechat/SelectUser.java
@@ -1,25 +1,91 @@
 package com.chat.app.firebasechat;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class SelectUser extends ActionBarActivity implements OnClickListener {
 
-	
+	Spinner spinner;
+	int[] id;
+	List<String> list;
+	ListView list1;
+	ArrayAdapter<String> dataAdapter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.listuser);
+		id = new int[3];
+		list1 = (ListView) findViewById(R.id.list1);
+		list = new ArrayList<String>();
+		dataAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, list);
+		id[0] = 1;
+		id[1] = 2;
+		id[2] = 3;
+
+		list.add("mantra");
+		list.add("marrily");
+		list.add("touchkin");
+		dataAdapter.notifyDataSetChanged();
+
+		dataAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		list1.setAdapter(dataAdapter);
+		list1.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				String yob = parent.getAdapter().getItem(position).toString();
+				Intent intent = new Intent(SelectUser.this, MainActivity.class);
+				intent.putExtra("id", position);
+				intent.putExtra("group", yob);
+				startActivity(intent);
+				Log.d("yob", yob+" "+position);
+			}
+		});
+
+//			@Override
+//			public void onItemSelected(AdapterView<?> parent, View view,
+//					int position, long id) {
+//
+//				String yob = parent.getSelectedItem().toString();
+//				Intent intent = new Intent(SelectUser.this, MainActivity.class);
+//				intent.putExtra("id", position);
+//				intent.putExtra("group", yob);
+//				startActivity(intent);
+//				Log.d("yob", yob+" "+position);
+//			}
+//
+//			@Override
+//			public void onNothingSelected(AdapterView<?> parent) {
+//				// TODO Auto-generated method stub
+//
+//			}
+
 	}
+
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
